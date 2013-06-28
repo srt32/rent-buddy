9705bd6c0433533ede929c7c5929a0b5448cdc9a
Simon <simon.taranto@gmail.com>
2013-06-27 20:34:13 -0600

    initial commit, yeah

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RentBuddy::Application.config.secret_token = '9e21a4c5692cae9c136c76a92927b9e778f69a798ccc43ab0719de6fddd6195411619452a00c16eab860cfa6c16653cecad15871081ba84e8802edc8a8955bb9'
